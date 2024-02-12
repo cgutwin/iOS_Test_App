620237f618d56ff32140340af402fc85562ca73a
cgutwin <chris@cgutwin.ca>
2024-02-11 16:20:09 -0800

    Initial Commit

---
+++ test_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test_app
+//
+//  Created by Chris Gutwin on 2024-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test_app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test_app
+//
+//  Created by Chris Gutwin on 2024-02-11.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
